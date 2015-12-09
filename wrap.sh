b39ae9976f5d3600ff0c389f561e25e407b15013
Keith Beattie <KSBeattie@lbl.gov>
2015-12-09 14:36:42 -0800

    Renaming quick_tomopy to fast_tomopy

--- wrap.sh
+++ wrap.sh
@@ -12,7 +12,7 @@ function template {
 #SBATCH -N 1
 #SBATCH -A als
 #SBATCH -t 00:15:00
-#SBATCH -J quick_tomopy_%j
+#SBATCH -J fast_tomopy_%j
 #DW jobdw capacity=1GB access_mode=striped type=scratch
 #DW stage_in source=$DATIN destination=\$DW_JOB_STRIPED/$DATINBASE type=file
 #DW stage_out source=\$DW_JOB_STRIPED\outputdir destination=$DATOUT type=directory
@@ -22,7 +22,7 @@ source activate bl832
 
 mkdir \$DW_JOB_STRIPED\outputdir
 
-srun -N 1 -n 1 -- python quick_tomopy.py -i \$DW_JOB_STRIPED/$DATINBASE -o \$DW_JOB_STRIPED/outputdir/
+srun -N 1 -n 1 -- python fast_tomopy.py -i \$DW_JOB_STRIPED/$DATINBASE -o \$DW_JOB_STRIPED/outputdir/
 EOF
 }
 
